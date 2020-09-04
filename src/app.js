09a236c362eb56bba9a2a72ccb88c04c26b957e9
HiTrop <48996329+hitrop@users.noreply.github.com>
2020-09-04 21:24:13 +0530

    Revert to last stable commit

--- src/app.js
+++ src/app.js
@@ -8,11 +8,8 @@ import dotenv from 'dotenv';
 import DBPool from './configs/database'; 
 //routes
 import userRouter from './routes/users';
-import actionRouter from './routes/actions';
-import contactFormRouter from './routes/contactform';
 //models
 import User from './models/User';
-import ContactForm from './models/ContactForm';
 
 //setup process env
 dotenv.config();
@@ -32,8 +29,6 @@ app.use(cookieParser());
 
 //setup routes and apis
 app.use('/api/users', userRouter);
-app.use('/api/actions', actionRouter);
-app.use('/api/contact', contactFormRouter);
 
 //404
 app.use(function(req, res, next) {
@@ -42,7 +37,7 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
-    res.send("WRONG LOL");
+    res.send("ERROR!");
 });
 
 //start the server on db connection
@@ -53,7 +48,6 @@ DBPool.initDB(dbURL).then((db)=>{
     app.listen(port, ()=>{
         console.log('Server listening on '+port);
         User.injectDB(db);
-        ContactForm.injectDB(db);
     });
 }).catch(err=>{
     console.log('DB Connection failure, hence server start failed!');
--- src/routes/actions.js
+++
@@ -1,5 +0,0 @@
-import express from 'express';
-const router = new express.Router();
-
-
-export default router;
